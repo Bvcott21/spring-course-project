0da588d5b0ab04886bb5406328f4a7b2ae3b62d9
edgar.afonso <edgar.afonso@fdmgroup.com>
2024-05-07 14:40:46 +0100

    Included post functionality

--- src/main/java/com/bucott/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/bucott/restfulwebservices/user/UserDaoService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoService {
 	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
 	
 	static {
 		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
@@ -21,10 +22,16 @@ public class UserDaoService {
 	}
 	
 	public User save(User user) {
-		return null;
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 	
-	public User findOne() {
-		return null;
+	public User findOne(int id) {
+		return users
+			.stream()
+			.filter(user -> user.getId().equals(id))
+			.findFirst()
+			.get();
 	}
 }
