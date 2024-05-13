08df4c947d129451e0180e91a5eb3f3779e44682
edgar.afonso <edgar.afonso@fdmgroup.com>
2024-05-13 10:43:01 +0100

    Improved validation messages

--- src/main/java/com/bucott/restfulwebservices/user/User.java
+++ src/main/java/com/bucott/restfulwebservices/user/User.java
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.Size;
 
 public class User {
 	private Integer id;
-	@Size(min = 2) private String name;	
-	@Past private LocalDate birthDate;
+	@Size(min = 2, message = "Name should have at least 2 characters.") private String name;	
+	@Past(message = "Birth Date should be in the past.") private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
