6e492212b96c1e8e1fb958539aedee943086710d
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2024-05-14 16:08:23 +0100

    Demonstrated further ways of versioning

--- src/main/java/com/bucott/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/bucott/restfulwebservices/versioning/VersioningPersonController.java
@@ -5,13 +5,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+	
+	// To access different versions of the API's endpoints via URI - Twitter 
 	@GetMapping("/v1/person")
 	public PersonV1 getFirstVersionOfPerson() {
-		return new PersonV1("Bob Charle");
+		return new PersonV1("Bob Charlie");
 	}
 	
 	@GetMapping("/v2/person")
 	public PersonV2 getSecondVersionOfPerson() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	// To access different versions of the API's endpoints via params - Amazon
+	@GetMapping(path="/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person", params="version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	// To access different versions of the API's endpoints via headers - Microsoft
+	@GetMapping(path="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	// To access different versions of the API's endpoints via MediaType - GitHub
+	@GetMapping(path="/person/accept", produces="application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonRequestMediaType() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path="/person/accept", produces="application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonRequestMediaType() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
 }
